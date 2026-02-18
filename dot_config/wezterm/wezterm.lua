63674192f65c80834c70ab2fb7f4e19972c3a176
hstorz <hstorz@proton.me>
2026-02-18 12:04:32 +0100

    Add is_mac variable for better future usage

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
+local is_mac = wezterm.target_triple:find('darwin') ~= nil
 
 config = {
     automatically_reload_config = true,
     hide_tab_bar_if_only_one_tab = true,
     window_close_confirmation = 'NeverPrompt',
-    window_decorations = wezterm.target_triple:find('linux') ~= nil and "RESIZE | TITLE | INTEGRATED_BUTTONS" or "RESIZE | TITLE",
+    window_decorations = is_mac and "RESIZE | TITLE" or "RESIZE | TITLE | INTEGRATED_BUTTONS",
     default_cursor_style = "BlinkingBar",
     enable_scroll_bar = true,
     enable_wayland = true,
