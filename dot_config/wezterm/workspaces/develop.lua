d89c7e73c5ebd61fd98d2d39b0b52453afc83010
hstorz <1732435+hstorz@users.noreply.github.com>
2026-02-19 00:37:13 +0100

    Improve develop wezterm workspace pane splits

--- dot_config/wezterm/workspaces/develop.lua
+++ dot_config/wezterm/workspaces/develop.lua
@@ -1,30 +1,30 @@
 local wezterm = require 'wezterm'
 
 local function setup(config)
-	
     -- Open workspace with shortcut
     table.insert(config.keys, {
         key = "d",
         mods = "CMD|SHIFT",
         action = wezterm.action_callback(function(window, pane)
-			
             -- Pane 1: fresh editor
             pane:send_text("fresh\n")
-            
-            -- Pane 2: opencode to the right
-            window:perform_action(wezterm.action.SplitPane({
-                direction = "Right",
-                size = { Percent = 50 }
-            }), pane)
-            
-            local opencode_pane = window:active_pane()
-            opencode_pane:send_text("opencode\n")
-            
-            -- Pane 3: terminal below opencode
+            local fresh_pane = pane
+
+            -- Pane 2: terminal below
             window:perform_action(wezterm.action.SplitPane({
                 direction = "Down",
-                size = { Percent = 50 }
-            }), opencode_pane)
+                size = { Percent = 25 }
+            }), fresh_pane)
+
+            -- Pane 3: opencode to the right
+            window:perform_action(wezterm.action.SplitPane({
+                direction = "Right",
+                size = { Percent = 35 },
+                top_level = true
+            }), fresh_pane)
+
+            local opencode_pane = window:active_pane()
+            opencode_pane:send_text("opencode\n")
         end)
     })
 end
