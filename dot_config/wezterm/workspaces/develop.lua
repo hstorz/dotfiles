0c8322f6af97dae7812b68fc6dfd6ca34771bbb6
hstorz <1732435+hstorz@users.noreply.github.com>
2026-02-19 22:59:01 +0100

    Change workspace layout

--- dot_config/wezterm/workspaces/develop.lua
+++ dot_config/wezterm/workspaces/develop.lua
@@ -10,21 +10,24 @@ local function setup(config)
             pane:send_text("fresh\n")
             local fresh_pane = pane
 
-            -- Pane 2: terminal below
+            -- Pane 2: opencode below
             window:perform_action(wezterm.action.SplitPane({
                 direction = "Down",
-                size = { Percent = 25 }
-            }), fresh_pane)
-
-            -- Pane 3: opencode to the right
-            window:perform_action(wezterm.action.SplitPane({
-                direction = "Right",
-                size = { Percent = 35 },
-                top_level = true
+                size = { Percent = 50 }
             }), fresh_pane)
 
             local opencode_pane = window:active_pane()
             opencode_pane:send_text("opencode\n")
+
+            -- Pane 3: lazygit to the right
+            window:perform_action(wezterm.action.SplitPane({
+                direction = "Right",
+                size = { Percent = 30 },
+                top_level = true
+            }), fresh_pane)
+
+            local opencode_pane = window:active_pane()
+            opencode_pane:send_text("lazygit\n")
         end)
     })
 end
